0cec8a1b379c6e68dd03feb37261f26c2ef206f2
Volodymyr_Kolodii <kolodijv80@gmail.com>
2023-04-29 03:15:47 +0300

    init commit

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 
-### First Run
+### First Run:
 `docker-compose up -d --build`
 
-####project url 
+##### Connect to DB in Workbench, then copy and run sql scripts from sql dir (1- cabmin.sql, 2-insert.sql)
+
+#### project url: 
 127.0.0.1:8080
--- public/db/databaseconnect.php
+++ public/db/databaseconnect.php
@@ -4,7 +4,9 @@ $database = getenv('MYSQL_DATABASE');
 $username = getenv('MYSQL_USER');
 $password = getenv('MYSQL_PASSWORD');
 $port = getenv('MYSQL_PORT');
-$mysqli = new mysqli('mysql8-container-my_app',$username,$password,$database, $port);
+$name = getenv('DOCKER_PROJECT_NAME');
+$hostname = 'mysql8-container-' . $name;
+$mysqli = new mysqli($hostname,$username,$password,$database, $port);
 
 //$db = new PDO('mysql:host=mysql8-container-my_app;port=3306;dbname=cabinet_of_ministers', $username, $password);
 if ($mysqli -> connect_errno) {
